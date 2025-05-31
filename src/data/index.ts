969c4a6bac5e7fff4d31d6f60c35068f47ab7008
Francisco <github.backstage538@passmail.net>
2025-05-31 19:44:10 +0200

    Cubes container

---
+++ src/data/index.ts
@@ -0,0 +1,13 @@
+export const blocks = [
+  'air',
+  'stone',
+  'grass',
+  'dirt',
+  'log',
+  'wood',
+  'leaves',
+  'glass',
+];
+export const layers = 9;
+export const rows = 9;
+export const columns = 9;
